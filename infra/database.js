12adbc151802e316356764fd53a298f6a06c5533
Jean Felipe Pinto Simões <jeanfeliz3@gmail.com>
2026-05-31 02:59:49 -0300

    make `database.js connect neon using ssl permission

--- infra/database.js
+++ infra/database.js
@@ -7,14 +7,9 @@ async function query(queryObject) {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     database: process.env.POSTGRES_DB,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
-  console.log("teste", {
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    database: process.env.POSTGRES_DB,
-  });
+
   try {
     await client.connect();
     const result = await client.query(queryObject);
